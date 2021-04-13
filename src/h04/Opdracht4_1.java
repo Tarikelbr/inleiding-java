2f4fb079c91568a57a4e19e8657f48fbac1068b9
jevader123 <tarikspowat15@gmail.com>
2021-04-13 14:01:47 +0200

    Opdracht 4_1 afgerond

---
+++ src/h04/Opdracht4_1.java
@@ -0,0 +1,29 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht4_1 extends Applet {
+
+
+    public void init() {
+    }
+
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.drawLine(50, 200, 200,200);
+        g.drawLine(50, 200, 130,50);
+        g.drawLine(130,50, 200, 200);
+    }
+}
+
+
+
+
+
+
+
+
+
+
