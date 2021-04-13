36c42e8fdb2337f215fb3b5600014dfbe8c95e89
jevader123 <tarikspowat15@gmail.com>
2021-04-13 12:07:03 +0200

    Opdracht 2.1 afgerond

---
+++ src/h02/Opdracht2_2.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2_2 extends Applet {
+
+    @Override
+    public void init() {
+
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("El Bachir", 60, 60 );
+    }
+}
