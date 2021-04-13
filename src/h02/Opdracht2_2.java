a54dba8efdf6bd47d4db54daa007c4e3e96b8952
jevader123 <tarikspowat15@gmail.com>
2021-04-13 12:10:53 +0200

    Opdracht 2.2 afgerond

--- src/h02/Opdracht2_2.java
+++ src/h02/Opdracht2_2.java
@@ -13,6 +13,9 @@ public class Opdracht2_2 extends Applet {
 
     @Override
     public void paint(Graphics g) {
-        g.drawString("El Bachir", 60, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Tarik", 60, 60 );
+        g.setColor(Color.red);
+        g.drawString("El Bachir", 60, 70 );
     }
 }
