6f31bca24867b1d8431a6a7a9a259d18796e1ae2
jevader123 <tarikspowat15@gmail.com>
2021-04-15 12:04:16 +0200

    Opdrachtpraktijk 4 afgerond

---
+++ src/h04/Opdracht4_4.java
@@ -0,0 +1,31 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht4_4 extends Applet {
+
+
+    public void init() {
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        g.drawRect(100, 150, 50, 210);
+        g.drawRect(150, 60,50,300);
+        g.drawRect(200,50,50,310);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
