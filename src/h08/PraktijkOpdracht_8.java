079d45b0d4d8da6962cb15f014c36bff7173f479
jevader123 <tarikspowat15@gmail.com>
2021-05-16 16:02:32 +0200

    Opdrachtpraktijk 8.3 afgerond

---
+++ src/h08/PraktijkOpdracht_8.java
@@ -0,0 +1,18 @@
+package h08;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class PraktijkOpdracht_8 extends Applet {
+TextField tekstvak1, tekstvak2;
+Button keerKnop, deelKnop, plusKnop, minKnop;
+double invoegGetal1, invoerGetal2, antwoord;
+
+
+
+    public void init() {
+        setSize(500, 100);
+        keerKnop = new Button("*");
+
+    }
+}
